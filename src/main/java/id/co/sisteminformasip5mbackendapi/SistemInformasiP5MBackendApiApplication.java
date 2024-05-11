fa44df4a82551136e6a49a7f6bee387875a9908e
xxayunayz <ayuurahmah@outlook.com>
2024-05-11 10:02:02 +0700

    Initial commit

---
+++ src/main/java/id/co/sisteminformasip5mbackendapi/SistemInformasiP5MBackendApiApplication.java
@@ -0,0 +1,13 @@
+package id.co.sisteminformasip5mbackendapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemInformasiP5MBackendApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SistemInformasiP5MBackendApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/id/co/sisteminformasip5mbackendapi/SistemInformasiP5MBackendApiApplicationTests.java
@@ -0,0 +1,13 @@
+package id.co.sisteminformasip5mbackendapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemInformasiP5MBackendApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
